0cb0fc805c7073ad6bef499208ced34c7d1442a3
Kévin Gomez <kevin.gomez@grafana.com>
2023-09-11 16:46:24 +0200

    Bootstrap txtar-based jennies tests

---
+++ internal/envvars/env.go
@@ -0,0 +1,11 @@
+package envvars
+
+import "os"
+
+// VarUpdateGolden is the name of the env var to trigger updating golden test files.
+const VarUpdateGolden = "COG_UPDATE_GOLDEN"
+
+// UpdateGoldenFiles determines whether tests should update txtar
+// archives on failures.
+// It is controlled by setting COG_UPDATE_GOLDEN to a non-empty string like "true".
+var UpdateGoldenFiles = os.Getenv(VarUpdateGolden) != ""
