077fcf3d9ac3cd202346ec7055b58e83ef349b42
Kévin Gomez <kevin.gomez@grafana.com>
2023-09-28 15:57:00 +0200

    Use txtar for cue generator tests

--- internal/simplecue/generator_test.go
+++ internal/simplecue/generator_test.go
@@ -1,14 +1,20 @@
 package simplecue
 
 import (
-	"cuelang.org/go/cue/cuecontext"
-	"cuelang.org/go/cue/load"
+	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/require"
 	"io"
 	"io/fs"
 	"path/filepath"
 	"testing"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/load"
+	"github.com/grafana/cog/internal/ast"
+	"github.com/grafana/cog/internal/txtartest"
+	"github.com/stretchr/testify/require"
+	"github.com/yalue/merged_fs"
 )
 
 // ToOverlay converts a fs.FS into a CUE loader overlay.
@@ -49,40 +55,74 @@ func toCueOverlay(prefix string, vfs fs.FS, overlay map[string]load.Source) erro
 	return nil
 }
 
-func TestTypes_TestCueGenerator(t *testing.T) {
-	//libFS := &merged_fs.EmptyFS{}
-	//overlay := make(map[string]load.Source)
-	//err := toCueOverlay("/", libFS, overlay)
-	//require.NoError(t, err)
+func TestGenerateAST(t *testing.T) {
+	test := txtartest.TxTarTest{
+		Root: "../../testdata/simplecue",
+		Name: "simplecue/GenerateAST",
+	}
 
-	bis := load.Instances([]string{"testdata/dashboard.cue"}, &load.Config{
-		//Overlay:    overlay,
+	test.Run(t, func(tc *txtartest.Test) {
+		req := require.New(tc)
+
+		schemaAst, err := GenerateAST(txtarTestToCueInstance(tc), Config{Package: "grafanatest"})
+		req.NoError(err)
+		require.NotNil(t, schemaAst)
+
+		writeIR(schemaAst, tc)
+	})
+}
+
+func writeIR(irFile *ast.File, tc *txtartest.Test) {
+	tc.Helper()
+
+	// TODO: use FileWriter instead in separate CL.
+	_, err := fmt.Fprintln(tc, "==", "ir.json")
+	require.NoError(tc, err)
+
+	marshaledIR, err := json.MarshalIndent(irFile, "", "  ")
+	require.NoError(tc, err)
+
+	_, err = tc.Write(marshaledIR)
+	require.NoError(tc, err)
+}
+
+func txtarTestToCueInstance(tc *txtartest.Test) cue.Value {
+	tc.Helper()
+
+	for _, f := range tc.Archive.Files {
+		if f.Name != "schema.cue" {
+			continue
+		}
+
+		return bytesToCueValue(tc.T, f.Data)
+	}
+
+	// the ir.json file wasn't found, let's fail hard.
+	tc.Fatal("could not load types IR: file 'ir.json' not found in test archive")
+
+	return cue.Value{}
+}
+
+func bytesToCueValue(t *testing.T, input []byte) cue.Value {
+	t.Helper()
+
+	req := require.New(t)
+
+	libFS := &merged_fs.EmptyFS{}
+	overlay := make(map[string]load.Source)
+	err := toCueOverlay("/", libFS, overlay)
+	req.NoError(err)
+
+	someSource := load.FromBytes(input)
+
+	overlay["/schema.cue"] = someSource
+
+	bis := load.Instances([]string{"/schema.cue"}, &load.Config{
+		Overlay:    overlay,
 		ModuleRoot: "/",
 	})
 	values, err := cuecontext.New().BuildInstances(bis)
-	require.NoError(t, err)
+	req.NoError(err)
 
-	schemaAst, err := GenerateAST(values[0], Config{Package: "grafanatest"})
-	require.NoError(t, err)
-	require.NotNil(t, schemaAst)
-	//
-	//testCases := []struct {
-	//	description string
-	//	expected    bool
-	//}{
-	//	{
-	//		description: "foo",
-	//		expected:    false,
-	//	},
-	//}
-	//
-	//for _, testCase := range testCases {
-	//	tc := testCase
-	//
-	//	t.Run(tc.description, func(t *testing.T) {
-	//		req := require.New(t)
-	//
-	//		req.Equal(tc.expected, false)
-	//	})
-	//}
+	return values[0]
 }
