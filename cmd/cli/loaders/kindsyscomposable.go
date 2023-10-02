45c28bafe1643fb33e498fcfb131f9f222166709
Kévin Gomez <kevin.gomez@grafana.com>
2023-10-02 12:42:02 +0200

    Support loading kindsys composable kinds

--- cmd/cli/loaders/kindsyscomposable.go
+++ cmd/cli/loaders/kindsyscomposable.go
@@ -11,11 +11,11 @@ import (
 	"github.com/grafana/thema"
 )
 
-func kindsysCustomLoader(opts Options) ([]*ast.File, error) {
+func kindsysCompopsableLoader(opts Options) ([]*ast.File, error) {
 	themaRuntime := thema.NewRuntime(cuecontext.New())
 
-	allSchemas := make([]*ast.File, 0, len(opts.KindsysCustomEntrypoints))
-	for _, entrypoint := range opts.KindsysCustomEntrypoints {
+	allSchemas := make([]*ast.File, 0, len(opts.KindsysComposableEntrypoints))
+	for _, entrypoint := range opts.KindsysComposableEntrypoints {
 		pkg := filepath.Base(entrypoint)
 
 		overlayFS, err := buildKindsysEntrypointFS(opts, entrypoint)
@@ -23,22 +23,22 @@ func kindsysCustomLoader(opts Options) ([]*ast.File, error) {
 			return nil, err
 		}
 
-		cueInstance, err := kindsys.BuildInstance(themaRuntime.Context(), ".", pkg, overlayFS)
+		cueInstance, err := kindsys.BuildInstance(themaRuntime.Context(), ".", "grafanaplugin", overlayFS)
 		if err != nil {
 			return nil, fmt.Errorf("could not load kindsys instance: %w", err)
 		}
 
-		props, err := kindsys.ToKindProps[kindsys.CustomProperties](cueInstance)
+		props, err := kindsys.ToKindProps[kindsys.ComposableProperties](cueInstance)
 		if err != nil {
 			return nil, fmt.Errorf("could not convert cue value to kindsys props: %w", err)
 		}
 
-		kindDefinition := kindsys.Def[kindsys.CustomProperties]{
+		kindDefinition := kindsys.Def[kindsys.ComposableProperties]{
 			V:          cueInstance,
 			Properties: props,
 		}
 
-		boundKind, err := kindsys.BindCustom(themaRuntime, kindDefinition)
+		boundKind, err := kindsys.BindComposable(themaRuntime, kindDefinition)
 		if err != nil {
 			return nil, fmt.Errorf("could not bind kind definition to kind: %w", err)
 		}
