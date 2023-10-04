9e2d3e06e07b2115e148aded445a6a70bf2aa1c0
spinillos <selenepinillos@gmail.com>
2023-10-04 12:03:21 +0200

    Add a few more tests

--- internal/openapi/generator_test.go
+++ internal/openapi/generator_test.go
@@ -32,7 +32,7 @@ func TestDataTypes(t *testing.T) {
 			name:       "int32",
 			kind:       ast.KindScalar,
 			scalarKind: ast.KindInt32,
-			defValue:   3,
+			defValue:   float64(3),
 		},
 		{
 			name:       "int64",
@@ -63,17 +63,156 @@ func TestDataTypes(t *testing.T) {
 		},
 	}
 	for _, tc := range testCases {
-		validateFields(t, structType, tc)
+		t.Run(tc.name, func(t *testing.T) {
+			validateFields(t, structType, tc)
+		})
+	}
+}
+
+func TestEnums(t *testing.T) {
+	f, err := GenerateAST(testFolder+"enums.json", Config{Package: "enums"})
+	require.NoError(t, err)
+
+	require.Len(t, f.Definitions, 1)
+
+	def := f.Definitions[0]
+	assert.Equal(t, def.Name, "Enums")
+	assert.Equal(t, def.Type.Kind, ast.KindStruct)
+
+	structType := def.Type.AsStruct()
+	testCases := []field{
+		{
+			name: "enumString",
+			kind: ast.KindEnum,
+			enumValues: []ast.EnumValue{
+				{
+					Type:  ast.NewScalar(ast.KindString),
+					Name:  "a",
+					Value: "a",
+				},
+				{
+					Type:  ast.NewScalar(ast.KindString),
+					Name:  "b",
+					Value: "b",
+				},
+			},
+			required: true,
+		},
+		{
+			name: "enumInt",
+			kind: ast.KindEnum,
+			enumValues: []ast.EnumValue{
+				{
+					Type:  ast.NewScalar(ast.KindInt64),
+					Name:  "3",
+					Value: float64(3),
+				},
+				{
+					Type:  ast.NewScalar(ast.KindInt64),
+					Name:  "4",
+					Value: float64(4),
+				},
+			},
+		},
+		{
+			name: "enumWithDefault",
+			kind: ast.KindEnum,
+			enumValues: []ast.EnumValue{
+				{
+					Type:  ast.NewScalar(ast.KindString),
+					Name:  "a",
+					Value: "a",
+				},
+				{
+					Type:  ast.NewScalar(ast.KindString),
+					Name:  "b",
+					Value: "b",
+				},
+			},
+			defValue: "a",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			validateFields(t, structType, tc)
+		})
+	}
+}
+
+func TestArrays(t *testing.T) {
+	f, err := GenerateAST(testFolder+"arrays.json", Config{Package: "arrays"})
+	require.NoError(t, err)
+
+	require.Len(t, f.Definitions, 2)
+	def := f.LocateDefinition("Arrays")
+	assert.Equal(t, def.Type.Kind, ast.KindStruct)
+
+	structType := def.Type.AsStruct()
+	testCases := []field{
+		{
+			name:          "arrayString",
+			kind:          ast.KindArray,
+			required:      true,
+			arrayTypeKind: ast.KindScalar,
+			scalarKind:    ast.KindString,
+		},
+		{
+			name:          "arrayInt",
+			kind:          ast.KindArray,
+			arrayTypeKind: ast.KindScalar,
+			scalarKind:    ast.KindInt64,
+		},
+		{
+			name:          "arrayRef",
+			kind:          ast.KindArray,
+			arrayTypeKind: ast.KindRef,
+			refType:       "Test",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			validateFields(t, structType, tc)
+		})
+	}
+}
+
+func TestRefs(t *testing.T) {
+	f, err := GenerateAST(testFolder+"refs.json", Config{Package: "refs"})
+	require.NoError(t, err)
+
+	require.Len(t, f.Definitions, 2)
+	def := f.LocateDefinition("Refs")
+	assert.Equal(t, def.Type.Kind, ast.KindStruct)
+
+	structType := def.Type.AsStruct()
+	testCases := []field{
+		{
+			name:     "ref",
+			kind:     ast.KindRef,
+			refType:  "Test",
+			required: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			validateFields(t, structType, tc)
+		})
 	}
 }
 
 type field struct {
-	name       string
-	kind       ast.Kind
-	scalarKind ast.ScalarKind
-	required   bool
-	nullable   bool
-	defValue   any
+	name          string
+	kind          ast.Kind
+	scalarKind    ast.ScalarKind
+	required      bool
+	nullable      bool
+	defValue      any
+	enumValues    []ast.EnumValue
+	refType       string
+	arrayTypeKind ast.Kind
 }
 
 func validateFields(t *testing.T, s ast.StructType, f field) {
@@ -85,5 +224,20 @@ func validateFields(t *testing.T, s ast.StructType, f field) {
 	assert.Equal(t, field.Required, f.required)
 	assert.Equal(t, field.Type.Default, f.defValue)
 	assert.Equal(t, field.Type.Nullable, f.nullable)
-	assert.Equal(t, field.Type.AsScalar().ScalarKind, f.scalarKind)
+
+	validateKind(t, field.Type, f)
+}
+
+func validateKind(t *testing.T, tp ast.Type, f field) {
+	switch tp.Kind {
+	case ast.KindScalar:
+		assert.Equal(t, tp.AsScalar().ScalarKind, f.scalarKind)
+	case ast.KindEnum:
+		assert.Equal(t, tp.AsEnum().Values, f.enumValues)
+	case ast.KindRef:
+		assert.Equal(t, tp.AsRef().ReferredType, f.refType)
+	case ast.KindArray:
+		assert.Equal(t, tp.AsArray().ValueType.Kind, f.arrayTypeKind)
+		validateKind(t, tp.AsArray().ValueType, f)
+	}
 }
