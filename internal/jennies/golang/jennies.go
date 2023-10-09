6123a8c407311a8b986c0f5478d0a0480b839442
spinillos <selenepinillos@gmail.com>
2023-10-09 11:07:58 +0200

    Undo test comment

--- internal/jennies/golang/jennies.go
+++ internal/jennies/golang/jennies.go
@@ -32,7 +32,7 @@ func Jennies() *codejen.JennyList[[]*ast.Schema] {
 			},
 		),
 	)
-	//targets.AddPostprocessors(PostProcessFile)
+	targets.AddPostprocessors(PostProcessFile)
 
 	return targets
 }
