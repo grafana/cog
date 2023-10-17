6cb014433b14873bc2cada9207f1e2a871e44278
Kévin Gomez <kevin.gomez@grafana.com>
2023-10-17 15:02:36 +0200

    Ensure undesired dashboard panel options are omitted from builders

--- internal/veneers/option/selectors.go
+++ internal/veneers/option/selectors.go
@@ -2,13 +2,14 @@ package option
 
 import (
 	"github.com/grafana/cog/internal/ast"
+	"github.com/grafana/cog/internal/tools"
 )
 
 type Selector func(builder ast.Builder, option ast.Option) bool
 
-func ByName(objectName string, optionName string) Selector {
+func ByName(objectName string, optionNames ...string) Selector {
 	return func(builder ast.Builder, option ast.Option) bool {
-		return builder.For.Name == objectName && option.Name == optionName
+		return builder.For.Name == objectName && tools.ItemInList(option.Name, optionNames)
 	}
 }
 
