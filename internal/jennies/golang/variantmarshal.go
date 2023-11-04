5468bd6166d19642076d2102ae88114a2b3deff7
Kévin Gomez <kevin.gomez@grafana.com>
2023-11-05 00:09:13 +0100

    tiny jennies reorg

--- internal/jennies/golang/variantmarshal.go
+++ internal/jennies/golang/variantmarshal.go
@@ -39,6 +39,8 @@ func (jenny VariantMarshalConfig) Generate(context context.Builders) (codejen.Fi
 		files = append(files, *codejen.NewFile(filename, output, jenny))
 	}
 
+	files = append(files, *codejen.NewFile("variants.go", jenny.variantRegistries(), jenny))
+
 	return files, nil
 }
 
@@ -60,8 +62,8 @@ func (jenny VariantMarshalConfig) dataqueryVariantInit(schema *ast.Schema) []byt
 
 	buffer.WriteString("func init() {\n")
 
-	buffer.WriteString(`cog.RegisterDataqueryVariant(cog.DataqueryVariantMarshalConfig{
-`)
+	buffer.WriteString(fmt.Sprintf(`cog.RegisterDataqueryVariant("%[1]s", cog.DataqueryVariantMarshalConfig{
+`, strings.ToLower(schema.Metadata.Identifier)))
 
 	variantObjFound := false
 	for _, obj := range schema.Objects {
@@ -82,6 +84,7 @@ func (jenny VariantMarshalConfig) dataqueryVariantInit(schema *ast.Schema) []byt
 	return dataquery, nil
 },
 `, tools.UpperCamelCase(obj.Name)))
+		break
 	}
 
 	if !variantObjFound {
@@ -150,4 +153,90 @@ func (jenny VariantMarshalConfig) panelcfgVariantInit(schema *ast.Schema) []byte
 	return []byte(fmt.Sprintf(`package %[1]s
 
 %[2]s%[3]s`, strings.ToLower(schema.Package), importStatements, buffer.String()))
+}
+
+func (jenny VariantMarshalConfig) variantRegistries() []byte {
+	return []byte(`package cog
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type PanelcfgVariantMarshalConfig struct {
+	OptionsUnmarshaler     func(raw []byte) (any, error)
+	FieldConfigUnmarshaler func(raw []byte) (any, error)
+}
+
+var panelcfgVariantsRegistry map[string]PanelcfgVariantMarshalConfig
+
+func RegisterPanelcfgVariant(variantName string, config PanelcfgVariantMarshalConfig) {
+	if panelcfgVariantsRegistry == nil {
+		panelcfgVariantsRegistry = make(map[string]PanelcfgVariantMarshalConfig)
+	}
+	panelcfgVariantsRegistry[variantName] = config
+}
+
+func MarshalConfigForPanelcfgVariant(variantName string) (PanelcfgVariantMarshalConfig, bool) {
+	config, found := panelcfgVariantsRegistry[variantName]
+
+	return config, found
+}
+
+type DataqueryVariantMarshalConfig struct {
+	DataqueryUnmarshaler func(raw []byte) (interface {
+	ImplementsDataqueryVariant()
+}, error)
+}
+
+var dataqueryVariantsRegistry map[string]DataqueryVariantMarshalConfig
+
+func RegisterDataqueryVariant(variantName string, config DataqueryVariantMarshalConfig) {
+	if dataqueryVariantsRegistry == nil {
+		dataqueryVariantsRegistry = make(map[string]DataqueryVariantMarshalConfig)
+	}
+	dataqueryVariantsRegistry[variantName] = config
+}
+
+func UnmarshalDataqueryArray[VariantT interface {
+	ImplementsDataqueryVariant()
+}](raw []byte) ([]VariantT, error) {
+	rawDataqueries := []json.RawMessage{}
+	if err := json.Unmarshal(raw, &rawDataqueries); err != nil {
+		return nil, err
+	}
+
+	dataqueries := make([]VariantT, 0, len(rawDataqueries))
+	for _, rawDataquery := range rawDataqueries {
+		dataquery, err := UnmarshalDataquery[VariantT](rawDataquery)
+		if err != nil {
+			return nil, err
+		}
+
+		dataqueries = append(dataqueries, dataquery)
+	}
+
+	return dataqueries, nil
+}
+
+func UnmarshalDataquery[VariantT any](raw []byte) (VariantT, error) {
+	var empty VariantT
+
+	for _, config := range dataqueryVariantsRegistry {
+		if config.DataqueryUnmarshaler == nil {
+			continue
+		}
+
+		dataquery, err := config.DataqueryUnmarshaler(raw)
+		if err != nil {
+			continue
+		}
+
+		return dataquery.(VariantT), nil
+	}
+
+	return empty, fmt.Errorf("could not unmarshal dataquery")
+}
+
+`)
 }
