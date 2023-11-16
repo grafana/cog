cd72d195223cb33ae353ef3fd671d22c55cd588f
Kévin Gomez <kevin.gomez@grafana.com>
2023-11-16 15:43:38 +0100

    Move jenny context struct

--- internal/jennies/common/context.go
+++ internal/jennies/common/context.go
@@ -1,20 +1,20 @@
-package context
+package common
 
 import (
 	"github.com/grafana/cog/internal/ast"
 )
 
 //nolint:musttag
-type Builders struct {
+type Context struct {
 	Schemas  ast.Schemas
 	Builders ast.Builders
 }
 
-func (context *Builders) LocateObject(pkg string, name string) (ast.Object, bool) {
+func (context *Context) LocateObject(pkg string, name string) (ast.Object, bool) {
 	return context.Schemas.LocateObject(pkg, name)
 }
 
-func (context *Builders) ResolveToBuilder(def ast.Type) bool {
+func (context *Context) ResolveToBuilder(def ast.Type) bool {
 	if def.Kind == ast.KindArray {
 		return context.ResolveToBuilder(def.AsArray().ValueType)
 	}
@@ -29,7 +29,7 @@ func (context *Builders) ResolveToBuilder(def ast.Type) bool {
 	return found
 }
 
-func (context *Builders) ResolveToComposableSlot(def ast.Type) (ast.Type, bool) {
+func (context *Context) ResolveToComposableSlot(def ast.Type) (ast.Type, bool) {
 	if def.Kind == ast.KindComposableSlot {
 		return def, true
 	}
--- internal/jennies/typescript/builder.go
+++ internal/jennies/typescript/builder.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/grafana/codejen"
 	"github.com/grafana/cog/internal/ast"
-	"github.com/grafana/cog/internal/jennies/context"
+	"github.com/grafana/cog/internal/jennies/common"
 	"github.com/grafana/cog/internal/jennies/template"
 	"github.com/grafana/cog/internal/tools"
 )
@@ -22,7 +22,7 @@ func (jenny *Builder) JennyName() string {
 	return "TypescriptBuilder"
 }
 
-func (jenny *Builder) Generate(context context.Builders) (codejen.Files, error) {
+func (jenny *Builder) Generate(context common.Context) (codejen.Files, error) {
 	files := codejen.Files{}
 
 	for _, builder := range context.Builders {
@@ -42,7 +42,7 @@ func (jenny *Builder) Generate(context context.Builders) (codejen.Files, error) 
 	return files, nil
 }
 
-func (jenny *Builder) generateBuilder(context context.Builders, builder ast.Builder) ([]byte, error) {
+func (jenny *Builder) generateBuilder(context common.Context, builder ast.Builder) ([]byte, error) {
 	var buffer strings.Builder
 
 	jenny.imports = template.NewImportMap()
