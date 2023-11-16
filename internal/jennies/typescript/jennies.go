9e854c18b4629ade5d48dca76c94ed2f4d951bbd
Kévin Gomez <kevin.gomez@grafana.com>
2023-11-16 14:51:49 +0100

    Treat language jennies as self-contained units

--- internal/jennies/typescript/jennies.go
+++ internal/jennies/typescript/jennies.go
@@ -4,11 +4,22 @@ import (
 	"github.com/grafana/codejen"
 	"github.com/grafana/cog/internal/ast/compiler"
 	"github.com/grafana/cog/internal/jennies/context"
+	"github.com/spf13/cobra"
 )
 
 const LanguageRef = "typescript"
 
-func Jennies() *codejen.JennyList[context.Builders] {
+type Language struct {
+}
+
+func New() *Language {
+	return &Language{}
+}
+
+func (language *Language) RegisterCliFlags(_ *cobra.Command) {
+}
+
+func (language *Language) Jennies() *codejen.JennyList[context.Builders] {
 	targets := codejen.JennyListWithNamer[context.Builders](func(_ context.Builders) string {
 		return LanguageRef
 	})
@@ -22,7 +33,7 @@ func Jennies() *codejen.JennyList[context.Builders] {
 	return targets
 }
 
-func CompilerPasses() compiler.Passes {
+func (language *Language) CompilerPasses() compiler.Passes {
 	return compiler.Passes{
 		&compiler.RenameNumericEnumValues{},
 	}
