8d34f8e57c96227ffac640c9617bbb0c427f82d2
Kévin Gomez <kevin.gomez@grafana.com>
2023-11-28 12:22:32 +0100

    Automatically load the 'common' package from the kind-registry if it's there

--- cmd/cli/loaders/kindregistry.go
+++ cmd/cli/loaders/kindregistry.go
@@ -29,6 +29,21 @@ func kindRegistryLoader(opts Options) ([]*ast.Schema, error) {
 	newOpts.KindsysCoreEntrypoints = coreKindEntrypoints
 	newOpts.KindsysComposableEntrypoints = composableKindEntrypoints
 
+	commonPkgPath := kindRegistryKindPath(opts, "common")
+	commonPkgExists, err := dirExists(commonPkgPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not locate common package: %w", err)
+	}
+	if commonPkgExists {
+		newOpts.CueEntrypoints = append(newOpts.CueEntrypoints, commonPkgPath)
+		newOpts.CueImports = append(newOpts.CueImports, fmt.Sprintf("%s:%s", commonPkgPath, "github.com/grafana/grafana/packages/grafana-schema/src/common"))
+	}
+
+	cueSchemas, err := cueLoader(newOpts)
+	if err != nil {
+		return nil, err
+	}
+
 	coreSchemas, err := kindsysCoreLoader(newOpts)
 	if err != nil {
 		return nil, err
@@ -39,6 +54,7 @@ func kindRegistryLoader(opts Options) ([]*ast.Schema, error) {
 		return nil, err
 	}
 
+	allSchemas = append(allSchemas, cueSchemas...)
 	allSchemas = append(allSchemas, coreSchemas...)
 	allSchemas = append(allSchemas, composableSchemas...)
 
@@ -70,4 +86,17 @@ func locateEntrypoints(opts Options, kind string) ([]string, error) {
 	}
 
 	return results, nil
+}
+
+func dirExists(dir string) (bool, error) {
+	stat, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else if !stat.IsDir() {
+		return false, fmt.Errorf("'%s' is not a directory", dir)
+	}
+
+	return true, nil
 }
