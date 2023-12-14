21ea6c83c875882dbf3fcdd729dc33fd86539525
Kévin Gomez <kevin.gomez@grafana.com>
2023-12-14 14:06:09 +0100

    Do not generate a constructor arguments for constant scalar fields

--- internal/jennies/python/rawtypes.go
+++ internal/jennies/python/rawtypes.go
@@ -97,7 +97,10 @@ func (jenny RawTypes) generateToInitMethod(schemas ast.Schemas, object ast.Objec
 		fieldType := jenny.typeFormatter.formatType(field.Type)
 		defaultValue := defaultValueForType(schemas, field.Type, jenny.importModule)
 
-		if field.Type.IsAnyOf(ast.KindStruct, ast.KindRef, ast.KindEnum, ast.KindMap, ast.KindArray) || field.Type.IsAny() {
+		if field.Type.IsScalar() && field.Type.AsScalar().IsConcrete() {
+			assignments = append(assignments, fmt.Sprintf("        self.%s = %s", fieldName, formatValue(field.Type.AsScalar().Value)))
+			continue
+		} else if field.Type.IsAnyOf(ast.KindStruct, ast.KindRef, ast.KindEnum, ast.KindMap, ast.KindArray) || field.Type.IsAny() {
 			if !field.Type.Nullable {
 				typingPkg := jenny.importPkg("typing", "typing")
 				fieldType = fmt.Sprintf("%s.Optional[%s]", typingPkg, fieldType)
