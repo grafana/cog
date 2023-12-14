567fa51c26f9207e24ed2becbcca2c0808926e7a
Kévin Gomez <kevin.gomez@grafana.com>
2023-12-14 13:56:25 +0100

    Generate constructors for python types

--- internal/jennies/python/rawtypes.go
+++ internal/jennies/python/rawtypes.go
@@ -1,17 +1,20 @@
 package python
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 
 	"github.com/grafana/codejen"
 	"github.com/grafana/cog/internal/ast"
 	"github.com/grafana/cog/internal/jennies/common"
+	"github.com/grafana/cog/internal/tools"
 )
 
 type RawTypes struct {
 	typeFormatter *typeFormatter
 	importModule  moduleImporter
+	importPkg     pkgImporter
 }
 
 func (jenny RawTypes) JennyName() string {
@@ -22,7 +25,7 @@ func (jenny RawTypes) Generate(context common.Context) (codejen.Files, error) {
 	files := make(codejen.Files, 0, len(context.Schemas))
 
 	for _, schema := range context.Schemas {
-		output, err := jenny.generateSchema(schema)
+		output, err := jenny.generateSchema(context, schema)
 		if err != nil {
 			return nil, err
 		}
@@ -35,7 +38,7 @@ func (jenny RawTypes) Generate(context common.Context) (codejen.Files, error) {
 	return files, nil
 }
 
-func (jenny RawTypes) generateSchema(schema *ast.Schema) ([]byte, error) {
+func (jenny RawTypes) generateSchema(context common.Context, schema *ast.Schema) ([]byte, error) {
 	var buffer strings.Builder
 
 	imports := NewImportMap()
@@ -46,13 +49,14 @@ func (jenny RawTypes) generateSchema(schema *ast.Schema) ([]byte, error) {
 
 		return imports.AddModule(alias, pkg, module)
 	}
-	jenny.typeFormatter = defaultTypeFormatter(func(alias string, pkg string) string {
+	jenny.importPkg = func(alias string, pkg string) string {
 		if strings.TrimPrefix(pkg, ".") == schema.Package {
 			return ""
 		}
 
 		return imports.AddPackage(alias, pkg)
-	}, jenny.importModule)
+	}
+	jenny.typeFormatter = defaultTypeFormatter(jenny.importPkg, jenny.importModule)
 
 	for i, object := range schema.Objects {
 		objectOutput, err := jenny.typeFormatter.formatObject(object)
@@ -61,6 +65,11 @@ func (jenny RawTypes) generateSchema(schema *ast.Schema) ([]byte, error) {
 		}
 
 		buffer.WriteString(objectOutput)
+
+		if object.Type.Kind == ast.KindStruct {
+			buffer.WriteString("\n\n")
+			buffer.Write([]byte(jenny.generateToInitMethod(context.Schemas, object)))
+		}
 
 		// we want two blank lines between objects, except at the end of the file
 		if i != len(schema.Objects)-1 {
@@ -76,4 +85,37 @@ func (jenny RawTypes) generateSchema(schema *ast.Schema) ([]byte, error) {
 	}
 
 	return []byte(importStatements + buffer.String()), nil
+}
+
+func (jenny RawTypes) generateToInitMethod(schemas ast.Schemas, object ast.Object) string {
+	var buffer strings.Builder
+
+	var args []string
+	var assignments []string
+
+	for _, field := range object.Type.AsStruct().Fields {
+		argName := tools.SnakeCase(escapeFieldName(field.Name))
+		fieldName := escapeFieldName(field.Name)
+		fieldType := jenny.typeFormatter.formatType(field.Type)
+		defaultValue := defaultValueForType(schemas, field.Type, jenny.importModule)
+
+		if field.Type.IsAnyOf(ast.KindStruct, ast.KindRef, ast.KindEnum) {
+			if !field.Type.Nullable {
+				typingPkg := jenny.importPkg("typing", "typing")
+				fieldType = fmt.Sprintf("%s.Optional[%s]", typingPkg, fieldType)
+			}
+
+			args = append(args, fmt.Sprintf("%s: %s = None", argName, fieldType))
+			assignments = append(assignments, fmt.Sprintf("        self.%[1]s = %[2]s if %[2]s is not None else %[3]s", fieldName, argName, formatValue(defaultValue)))
+			continue
+		}
+
+		args = append(args, fmt.Sprintf("%s: %s = %s", argName, fieldType, formatValue(defaultValue)))
+		assignments = append(assignments, fmt.Sprintf("        self.%s = %s", fieldName, argName))
+	}
+
+	buffer.WriteString(fmt.Sprintf("    def __init__(self, %s):\n", strings.Join(args, ", ")))
+	buffer.WriteString(strings.Join(assignments, "\n"))
+
+	return strings.TrimSuffix(buffer.String(), "\n")
 }
