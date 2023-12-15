ac1361184752d8176403c415ed94b6bf1bae4e26
Kévin Gomez <kevin.gomez@grafana.com>
2023-12-15 16:08:43 +0100

    Add log-related panels to the python example

--- examples/python/raspberry/common.py
+++ examples/python/raspberry/common.py
@@ -1,7 +1,10 @@
 from generated.builders.timeseries import Panel as TimeseriesBuilder
 from generated.builders.gauge import Panel as GaugeBuilder
+from generated.builders.logs import Panel as LogsBuilder
 from generated.builders.prometheus import Dataquery as PrometheusQueryBuilder
-from generated.models.common import GraphDrawStyle, VisibilityMode, LegendPlacement, LegendDisplayMode, VizOrientation
+from generated.builders.loki import Dataquery as LokiQueryBuilder
+from generated.models.dashboard import DataSourceRef
+from generated.models.common import GraphDrawStyle, VisibilityMode, LegendPlacement, LegendDisplayMode, VizOrientation, LogsSortOrder
 from generated.builders.common import VizLegendOptions as VizLegendOptionsBuilder, ReduceDataOptions as ReduceDataOptionsBuilder
 
 
@@ -18,6 +21,18 @@ def default_timeseries() -> TimeseriesBuilder:
             .placement(LegendPlacement.BOTTOM)
             .display_mode(LegendDisplayMode.LIST)
         )
+    )
+
+
+def default_logs() -> LogsBuilder:
+    return (
+        LogsBuilder()
+        .span(24)
+        .datasource(DataSourceRef(type_val="loki", uid="grafanacloud-logs"))
+        .show_time(True)
+        .enable_log_details(True)
+        .sort_order(LogsSortOrder.DESCENDING)
+        .wrap_log_message(True)
     )
 
 
@@ -39,4 +54,8 @@ def basic_prometheus_query(query: str, legend: str) -> PrometheusQueryBuilder:
         .expr(query)
         .legend_format(legend)
     )
+
+
+def basic_loki_query(query: str) -> LokiQueryBuilder:
+    return LokiQueryBuilder().expr(query)
 
