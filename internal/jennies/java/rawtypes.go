dd69e35fc43a9588ccbff3d6f8019017c7ee07e1
spinillos <selenepinillos@gmail.com>
2024-01-09 18:55:52 +0100

    Add intersections and tests

--- internal/jennies/java/rawtypes.go
+++ internal/jennies/java/rawtypes.go
@@ -89,7 +89,6 @@ func (jenny RawTypes) genFilesForSchema(schema *ast.Schema) (codejen.Files, erro
 }
 
 func (jenny RawTypes) generateSchema(pkg string, object ast.Object) ([]byte, error) {
-
 	switch object.Type.Kind {
 	case ast.KindStruct:
 		return jenny.formatStruct(pkg, object)
@@ -97,6 +96,8 @@ func (jenny RawTypes) generateSchema(pkg string, object ast.Object) ([]byte, err
 		return jenny.formatEnum(pkg, object)
 	case ast.KindRef:
 		return jenny.formatReference(pkg, object)
+	case ast.KindIntersection:
+		return jenny.formatIntersection(pkg, object)
 	}
 
 	return nil, nil
@@ -186,6 +187,10 @@ func (jenny RawTypes) formatScalars(pkg string, scalars map[string]ast.ScalarTyp
 		}
 	}
 
+	if len(constants) == 0 {
+		return nil, nil
+	}
+
 	if err := templates.ExecuteTemplate(&buffer, "types/constants.tmpl", ConstantTemplate{
 		Package:   pkg,
 		Name:      "Constants",
@@ -213,6 +218,50 @@ func (jenny RawTypes) formatReference(pkg string, object ast.Object) ([]byte, er
 	}
 
 	return []byte(buffer.String()), nil
+}
+
+func (jenny RawTypes) formatIntersection(pkg string, object ast.Object) ([]byte, error) {
+	var buffer strings.Builder
+
+	intersection := object.Type.AsIntersection()
+	extensions := make([]string, 0)
+	fields := make([]Field, 0)
+
+	for _, branch := range intersection.Branches {
+		switch branch.Kind {
+		case ast.KindRef:
+			extensions = append(extensions, jenny.typeFormatter.formatReference(branch.AsRef()))
+		case ast.KindStruct:
+			fields = append(fields, jenny.formatFields(branch.AsStruct())...)
+		}
+	}
+
+	if err := templates.ExecuteTemplate(&buffer, "types/class.tmpl", ClassTemplate{
+		Package:  pkg,
+		Imports:  jenny.imports,
+		Name:     object.Name,
+		Extends:  extensions,
+		Comments: object.Comments,
+		Fields:   fields,
+		Variant:  tools.UpperCamelCase(object.Type.ImplementedVariant()),
+	}); err != nil {
+		return nil, err
+	}
+
+	return []byte(buffer.String()), nil
+}
+
+func (jenny RawTypes) formatFields(def ast.StructType) []Field {
+	fields := make([]Field, len(def.Fields))
+	for i, field := range def.Fields {
+		fields[i] = Field{
+			Name:     field.Name,
+			Type:     jenny.typeFormatter.formatFieldType(field.Type),
+			Comments: field.Comments,
+		}
+	}
+
+	return fields
 }
 
 // TODO: Need to say to the serializer the correct name.
---
+++ internal/jennies/java/rawtypes_test.go
@@ -0,0 +1,38 @@
+package java
+
+import (
+	"github.com/grafana/cog/internal/ast"
+	"github.com/grafana/cog/internal/jennies/common"
+	"github.com/grafana/cog/internal/txtartest"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRawTypes_Generate(t *testing.T) {
+	test := txtartest.TxTarTest{
+		Root: "../../../testdata/jennies/rawtypes",
+		Name: "jennies/JavaRawTypes",
+	}
+
+	jenny := RawTypes{}
+	compilerPasses := New().CompilerPasses()
+
+	test.Run(t, func(tc *txtartest.Test) {
+		req := require.New(tc)
+
+		// We run the compiler passes defined fo Go since without them, we
+		// might not be able to translate some of the IR's semantics into Go.
+		// Example: disjunctions.
+		processedAsts, err := compilerPasses.Process(ast.Schemas{tc.TypesIR()})
+		req.NoError(err)
+
+		req.Len(processedAsts, 1, "we somehow got more ast.Schema than we put in")
+
+		files, err := jenny.Generate(common.Context{
+			Schemas: processedAsts,
+		})
+		req.NoError(err)
+
+		tc.WriteFiles(files)
+	})
+}
