da3e9bed2feb0a9f57e4f19e7174a12b2aef78b3
Kévin Gomez <kevin.gomez@grafana.com>
2024-01-31 15:04:35 +0100

    Support JSON (un)marshaling for orderedmaps

--- internal/orderedmap/map_test.go
+++ internal/orderedmap/map_test.go
@@ -1,6 +1,7 @@
 package orderedmap
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -28,4 +29,52 @@ func TestMap_Basic(t *testing.T) {
 	})
 
 	req.Equal([]string{"first-key", "second-key", "third-key"}, iteratedKeyOrder)
+}
+
+func TestMap_MarshalJSON(t *testing.T) {
+	req := require.New(t)
+	orderedMap := New[string, string]()
+
+	marshaled, err := json.Marshal(orderedMap)
+	req.NoError(err)
+	req.Equal(`{}`, string(marshaled))
+
+	orderedMap.Set("foo", "first")
+	orderedMap.Set("bar", "second")
+
+	marshaled, err = json.Marshal(orderedMap)
+	req.NoError(err)
+	req.Equal(`{"foo":"first","bar":"second"}`, string(marshaled))
+}
+
+func TestMap_UnmarshalJSON_empty(t *testing.T) {
+	req := require.New(t)
+	orderedMap := New[string, string]()
+
+	req.NoError(json.Unmarshal([]byte(`{}`), orderedMap))
+	req.Equal(0, orderedMap.Len())
+}
+
+func TestMap_UnmarshalJSON_string_to_string(t *testing.T) {
+	req := require.New(t)
+	orderedMap := New[string, string]()
+
+	req.NoError(json.Unmarshal([]byte(`{"foo":"first","bar":"second","aaa":"third"}`), orderedMap))
+	req.Equal(3, orderedMap.Len())
+	req.Equal("first", orderedMap.At(0))
+	req.Equal("second", orderedMap.At(1))
+	req.Equal("third", orderedMap.At(2))
+}
+
+func TestMap_UnmarshalJSON_string_to_struct(t *testing.T) {
+	req := require.New(t)
+	orderedMap := New[string, struct {
+		Title string `json:"title"`
+	}]()
+
+	req.NoError(json.Unmarshal([]byte(`{"foo":{"title":"first"},"bar":{"title":"second"},"aaa":{"title":"third"}}`), orderedMap))
+	req.Equal(3, orderedMap.Len())
+	req.Equal("first", orderedMap.At(0).Title)
+	req.Equal("second", orderedMap.At(1).Title)
+	req.Equal("third", orderedMap.At(2).Title)
 }
