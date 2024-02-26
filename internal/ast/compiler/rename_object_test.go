77908694752c8bdfc652c3fc3e127d3c87c6585a
Kévin Gomez <kevin.gomez@grafana.com>
2024-02-26 16:16:01 +0100

    Add "Rename Object" compiler pass

---
+++ internal/ast/compiler/rename_object_test.go
@@ -0,0 +1,44 @@
+package compiler
+
+import (
+	"testing"
+
+	"github.com/grafana/cog/internal/ast"
+	"github.com/grafana/cog/internal/testutils"
+)
+
+func TestRenameObject(t *testing.T) {
+	// Prepare test input
+	schema := &ast.Schema{
+		Package: "rename_object",
+		Objects: testutils.ObjectsMap(
+			ast.NewObject("rename_object", "SomeObject", ast.NewStruct(
+				ast.NewStructField("foo", ast.String(ast.Nullable())),
+				ast.NewStructField("ref_to_nice_object", ast.NewRef("rename_object", "NotANiceName")),
+			)),
+			ast.NewObject("rename_object", "NotANiceName", ast.NewStruct(
+				ast.NewStructField("AString", ast.String(ast.Nullable())),
+			)),
+		),
+	}
+	expected := &ast.Schema{
+		Package: "rename_object",
+		Objects: testutils.ObjectsMap(
+			ast.NewObject("rename_object", "SomeObject", ast.NewStruct(
+				ast.NewStructField("foo", ast.String(ast.Nullable())),
+				ast.NewStructField("ref_to_nice_object", ast.NewRef("rename_object", "ReallyNiceName")),
+			)),
+			ast.NewObject("rename_object", "ReallyNiceName", ast.NewStruct(
+				ast.NewStructField("AString", ast.String(ast.Nullable())),
+			)),
+		),
+	}
+
+	pass := &RenameObject{
+		From: ObjectReference{Package: schema.Package, Object: "NotANiceName"},
+		To:   "ReallyNiceName",
+	}
+
+	// Run the compiler pass
+	runPassOnSchema(t, pass, schema, expected)
+}
