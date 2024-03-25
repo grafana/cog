9e879f576e1432d04750a29b86ad290ee13084fb
Kévin Gomez <kevin.gomez@grafana.com>
2024-03-25 14:20:12 +0100

    Validate generated jsonschema if debug is enabled

--- internal/jennies/jsonschema/jennies.go
+++ internal/jennies/jsonschema/jennies.go
@@ -1,9 +1,13 @@
 package jsonschema
 
 import (
+	"bytes"
+	"strings"
+
 	"github.com/grafana/codejen"
 	"github.com/grafana/cog/internal/ast/compiler"
 	"github.com/grafana/cog/internal/jennies/common"
+	schemaparser "github.com/santhosh-tekuri/jsonschema"
 	"github.com/spf13/cobra"
 )
 
@@ -35,14 +39,15 @@ func (language *Language) RegisterCliFlags(_ *cobra.Command) {
 
 func (language *Language) Jennies(globalConfig common.Config) *codejen.JennyList[common.Context] {
 	config := language.config.MergeWithGlobal(globalConfig)
-
 	jenny := codejen.JennyListWithNamer[common.Context](func(_ common.Context) string {
 		return LanguageRef
 	})
 
-	jenny.AppendOneToMany(
-		Schema{Config: config},
-	)
+	jenny.AppendOneToMany(Schema{Config: config})
+
+	if config.Debug {
+		jenny.AddPostprocessors(ValidateSchemas)
+	}
 
 	return jenny
 }
@@ -52,4 +57,19 @@ func (language *Language) CompilerPasses() compiler.Passes {
 		&compiler.DisjunctionWithNullToOptional{},
 		&compiler.InferEntrypoint{},
 	}
+}
+
+func ValidateSchemas(file codejen.File) (codejen.File, error) {
+	if !strings.HasSuffix(file.RelativePath, ".json") {
+		return file, nil
+	}
+
+	schemaReader := bytes.NewReader(file.Data)
+	schemaCompiler := schemaparser.NewCompiler()
+
+	if err := schemaCompiler.AddResource("schema", schemaReader); err != nil {
+		return file, err
+	}
+
+	return file, nil
 }
