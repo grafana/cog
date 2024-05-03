7532466270760e2f2e6e6e0cc839e9185099aefc
Kévin Gomez <kevin.gomez@grafana.com>
2024-05-03 16:26:28 +0200

    Bump santhosh-tekuri/jsonschema to v5

--- internal/jsonschema/utils.go
+++ internal/jsonschema/utils.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"strings"
 
-	schemaparser "github.com/santhosh-tekuri/jsonschema"
+	schemaparser "github.com/santhosh-tekuri/jsonschema/v5"
 )
 
 func schemaComments(schema *schemaparser.Schema) []string {
