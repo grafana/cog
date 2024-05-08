fe996374be3f221bfe5c57870dddf61fac4601af
Kévin Gomez <kevin.gomez@grafana.com>
2024-05-08 16:24:29 +0200

    More alerting veneers

--- examples/_go_alerting/main.go
+++ examples/_go_alerting/main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/grafana/cog/generated/alerting"
-	"github.com/grafana/cog/generated/cog"
 	"github.com/grafana/cog/generated/cog/plugins"
 	"github.com/grafana/cog/generated/expr"
 	"github.com/grafana/cog/generated/prometheus"
@@ -20,7 +19,7 @@ func main() {
 
 	alert, err := alerting.NewRuleGroupBuilder("cog rule group").
 		Interval(alerting.Duration(interval.Seconds())). // type is confusing
-		Rule(alerting.NewRuleBuilder("cog alert rule name").
+		WithRule(alerting.NewRuleBuilder("cog alert rule name").
 			For("5m").
 			Annotations(map[string]string{
 				"summary":     "summary",
@@ -33,26 +32,24 @@ func main() {
 			Condition("B").
 			ExecErrState(alerting.RuleExecErrStateAlerting).
 			NoDataState(alerting.RuleNoDataStateOK).
-			Data([]cog.Builder[alerting.Query]{
-				alerting.NewQueryBuilder("A").
-					DatasourceUid("cdjvy8rnmh0qof").
-					RelativeTimeRange(600, 0). // TODO
-					Model(
-						prometheus.NewDataqueryBuilder().
-							// RefId("A"). // needed?
-							Expr("go_memstats_alloc_bytes_total").
-							Instant(true).
-							LegendFormat("__auto"),
-					),
-				alerting.NewQueryBuilder("B").
-					DatasourceUid("__expr__").
-					RelativeTimeRange(600, 0). // TODO
-					Model(
-						expr.NewTypeReduceBuilder().
-							Reducer("last").
-							Expression("A"),
-					),
-			}),
+			WithQuery(alerting.NewQueryBuilder("A").
+				DatasourceUid("cdjvy8rnmh0qof").
+				RelativeTimeRange(600, 0). // TODO
+				Model(
+					prometheus.NewDataqueryBuilder().
+						// RefId("A"). // needed?
+						Expr("go_memstats_alloc_bytes_total").
+						Instant(true).
+						LegendFormat("__auto"),
+				)).
+			WithQuery(alerting.NewQueryBuilder("B").
+				DatasourceUid("__expr__").
+				RelativeTimeRange(600, 0). // TODO
+				Model(
+					expr.NewTypeReduceBuilder().
+						Reducer("last").
+						Expression("A"),
+				)),
 		).
 		Build()
 	if err != nil {
