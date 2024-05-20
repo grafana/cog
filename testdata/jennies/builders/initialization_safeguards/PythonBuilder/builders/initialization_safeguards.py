912222322cdf1cd0816466ac3cd20075cb7a7614
Kévin Gomez <kevin.gomez@grafana.com>
2024-05-20 17:55:47 +0200

    Simplify assignment nil-checks in python

--- testdata/jennies/builders/initialization_safeguards/PythonBuilder/builders/initialization_safeguards.py
+++ testdata/jennies/builders/initialization_safeguards/PythonBuilder/builders/initialization_safeguards.py
@@ -17,17 +17,13 @@ class SomePanel(cogbuilder.Builder[initialization_safeguards.SomePanel]):
     
         return self
     
-    def show_legend(self, show: boolean) -> typing.Self:        
+    def show_legend(self, show: bool) -> typing.Self:        
         if self._internal.options is None:
             self._internal.options = initialization_safeguards.Options()
-        
         assert isinstance(self._internal.options, initialization_safeguards.Options)
-        
         if self._internal.options.legend is None:
             self._internal.options.legend = initialization_safeguards.LegendOptions()
-        
         assert isinstance(self._internal.options.legend, initialization_safeguards.LegendOptions)
-        
         self._internal.options.legend.show = show
     
         return self
