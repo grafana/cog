9a751c3ecf8bf0181e2d710c362feaa8e4401261
Kévin Gomez <kevin.gomez@grafana.com>
2024-05-24 10:57:49 +0200

    Remove dependencies on Masterminds/sprig

--- internal/jennies/golang/tmpl.go
+++ internal/jennies/golang/tmpl.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"text/template"
 
-	"github.com/Masterminds/sprig/v3"
 	"github.com/grafana/cog/internal/ast"
 	cogtemplate "github.com/grafana/cog/internal/jennies/template"
 )
@@ -23,7 +22,6 @@ func init() {
 	base := template.New("golang")
 	base.
 		Option("missingkey=error").
-		Funcs(sprig.FuncMap()).
 		Funcs(cogtemplate.Helpers(base)).
 		// placeholder functions, will be overridden by jennies
 		Funcs(template.FuncMap{
@@ -38,6 +36,12 @@ func init() {
 			},
 			"emptyValueForGuard": func(_ ast.Type) string {
 				panic("emptyValueForGuard() needs to be overridden by a jenny")
+			},
+			"typeHasBuilder": func(_ ast.Type) bool {
+				panic("typeHasBuilder() needs to be overridden by a jenny")
+			},
+			"resolvesToComposableSlot": func(_ ast.Type) bool {
+				panic("resolvesToComposableSlot() needs to be overridden by a jenny")
 			},
 		}).
 		Funcs(map[string]any{
