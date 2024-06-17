61d41420dfb59319415be290b8d87d1fb8e4328e
Kévin Gomez <kevin.gomez@grafana.com>
2024-06-17 14:30:42 +0200

    Disambiguate name conflicts in config schemas

--- cmd/cog-config-schemas/main.go
+++ cmd/cog-config-schemas/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
 	"strings"
 
 	"github.com/grafana/cog/internal/codegen"
+	"github.com/grafana/cog/internal/tools"
 	"github.com/grafana/cog/internal/yaml"
 	"github.com/invopop/jsonschema"
 )
@@ -21,20 +23,30 @@ func main() {
 		{name: "pipeline", input: &codegen.Pipeline{}},
 	}
 
+	reflector := &jsonschema.Reflector{
+		RequiredFromJSONSchemaTags: true, // we don't have good information as to what is required :/
+		FieldNameTag:               "yaml",
+		KeyNamer: func(key string) string {
+			if strings.ToUpper(string(key[0])) == string(key[0]) {
+				return strings.ToLower(key)
+			}
+
+			return key
+		},
+		Namer: func(reflected reflect.Type) string {
+			name := reflected.Name()
+
+			if reflected.PkgPath() != "" {
+				parts := strings.Split(reflected.PkgPath(), "/")
+				name = tools.UpperCamelCase(parts[len(parts)-1]) + tools.UpperCamelCase(name)
+			}
+
+			return name
+		},
+	}
+
 	for _, t := range types {
 		fmt.Printf("Generating schema for type '%s'\n", t.name)
-
-		reflector := &jsonschema.Reflector{
-			RequiredFromJSONSchemaTags: true, // we don't have good information as to what is required :/
-			FieldNameTag:               "yaml",
-			KeyNamer: func(key string) string {
-				if strings.ToUpper(string(key[0])) == string(key[0]) {
-					return strings.ToLower(key)
-				}
-
-				return key
-			},
-		}
 
 		if err := reflector.AddGoComments("github.com/grafana/cog", "./"); err != nil {
 			panic(fmt.Errorf("could not add Go comments to reflector: %w", err))
