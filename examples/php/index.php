b625718ecfc70776106155cb0ba07be03453c0ca
Kévin Gomez <kevin.gomez@grafana.com>
2024-06-17 15:09:09 +0200

    Implement jsonSerialize() for PHP classes

--- examples/php/index.php
+++ examples/php/index.php
@@ -2,6 +2,13 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-echo 'lala';
+echo 'lala'.PHP_EOL;
 
-$dashboard = new \Types\Dashboard\Dashboard();
+$dashboard = new \Grafana\Foundation\Types\Dashboard\Dashboard();
+$dashboard->schemaVersion = 1;
+$dashboard->templating = new \Grafana\Foundation\Types\Dashboard\DashboardDashboardTemplating();
+$dashboard->annotations = new \Grafana\Foundation\Types\Dashboard\AnnotationContainer();
+$dashboard->title = "Some dashboard";
+$dashboard->tags = ["foo", "bar"];
+
+var_dump(json_encode($dashboard));
