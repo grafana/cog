f61a3b71c25941dc50c9677cc1ee38250cb6e182
Kévin Gomez <kevin.gomez@grafana.com>
2024-06-17 16:34:04 +0200

    Add constructors for PHP classes

--- examples/php/index.php
+++ examples/php/index.php
@@ -4,10 +4,7 @@ require_once __DIR__.'/vendor/autoload.php';
 
 echo 'lala'.PHP_EOL;
 
-$dashboard = new \Grafana\Foundation\Types\Dashboard\Dashboard();
-$dashboard->schemaVersion = 1;
-$dashboard->templating = new \Grafana\Foundation\Types\Dashboard\DashboardDashboardTemplating();
-$dashboard->annotations = new \Grafana\Foundation\Types\Dashboard\AnnotationContainer();
+$dashboard = new \Grafana\Foundation\Types\Dashboard\Dashboard(uid: "foo");
 $dashboard->title = "Some dashboard";
 $dashboard->tags = ["foo", "bar"];
 
