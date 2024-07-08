c7a2e989d5a1a4986bf8c482516cb5d68c688caa
spinillos <selenepinillos@gmail.com>
2024-07-08 13:01:43 +0200

    Fix enum unmarshalling

--- testdata/jennies/rawtypes/enums/JavaRawTypes/enums/LogsSortOrder.java
+++ testdata/jennies/rawtypes/enums/JavaRawTypes/enums/LogsSortOrder.java
@@ -1,16 +1,21 @@
 package enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
 
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum LogsSortOrder {
     ASC("time_asc"),
     DESC("time_desc");
 
-    private String value;
+    private final String value;
 
     private LogsSortOrder(String value) {
         this.value = value;
     }
 
+    @JsonValue
     public String Value() {
         return value;
     }
--- testdata/jennies/rawtypes/enums/JavaRawTypes/enums/TableSortOrder.java
+++ testdata/jennies/rawtypes/enums/JavaRawTypes/enums/TableSortOrder.java
@@ -1,16 +1,21 @@
 package enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
 
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum TableSortOrder {
     ASC("asc"),
     DESC("desc");
 
-    private String value;
+    private final String value;
 
     private TableSortOrder(String value) {
         this.value = value;
     }
 
+    @JsonValue
     public String Value() {
         return value;
     }
