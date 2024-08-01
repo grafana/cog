bb1ad5e245a04a3ad17c5efcb846a9911161dd93
spinillos <selenepinillos@gmail.com>
2024-08-01 13:48:18 +0200

    Update to variants in generator

--- internal/jennies/golang/tools.go
+++ internal/jennies/golang/tools.go
@@ -8,6 +8,10 @@ import (
 )
 
 func formatPackageName(pkg string) string {
+	splitPath := strings.Split(pkg, "/")
+	if len(splitPath) > 1 {
+		pkg = splitPath[len(splitPath)-1]
+	}
 	rgx := regexp.MustCompile("[^a-zA-Z0-9_]+")
 
 	return strings.ToLower(rgx.ReplaceAllString(pkg, ""))
