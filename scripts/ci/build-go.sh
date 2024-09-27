6e9b1fada88110bd5300a234e2b181fea2ee5d67
Kévin Gomez <kevin.gomez@grafana.com>
2024-09-27 02:18:40 +0200

    Don't run 'devbox run' in a loop in the CI

---
+++ scripts/ci/build-go.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
+set -o pipefail
+
+for d in generated/go/*/ ; do
+  echo "Building $d"
+  go build "./$d"
+done
