02d4c6a2d9fdca5e8371a17d42c995978127c64f
Kévin Gomez <kevin.gomez@grafana.com>
2024-10-14 11:43:38 +0200

    Generate `Validate() error` function on Go types

--- internal/jennies/golang/tmpl.go
+++ internal/jennies/golang/tmpl.go
@@ -63,8 +63,14 @@ func initTemplates(extraTemplatesDirectories []string) *template.Template {
 			"resolvesToEnum": func(typeDef ast.Type) bool {
 				panic("refResolvesToEnum() needs to be overridden by a jenny")
 			},
+			"resolvesToStruct": func(typeDef ast.Type) bool {
+				panic("resolvesToStruct() needs to be overridden by a jenny")
+			},
 			"resolveRefs": func(typeDef ast.Type) ast.Type {
 				panic("resolveRefs() needs to be overridden by a jenny")
+			},
+			"resolvesToConstraints": func(_ ast.Type) string {
+				panic("resolvesToConstraints() needs to be overridden by a jenny")
 			},
 		}),
 		template.Funcs(map[string]any{
