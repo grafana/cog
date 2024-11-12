ba59db28fbdc7c75b537bc46d3064306c3b78b4f
spinillos <selenepinillos@gmail.com>
2024-11-12 13:16:14 +0100

    Comment converters to avoid to publish them to foundation-sdk

--- internal/jennies/java/jennies.go
+++ internal/jennies/java/jennies.go
@@ -72,7 +72,7 @@ func (language *Language) Jennies(globalConfig languages.Config) *codejen.JennyL
 		common.If[languages.Context](!config.SkipRuntime, &Serializers{config: config, tmpl: tmpl}),
 		RawTypes{config: config, tmpl: tmpl},
 		common.If[languages.Context](!config.SkipGradleDev, Gradle{config: config, tmpl: tmpl}),
-		common.If[languages.Context](!config.SkipRuntime && globalConfig.Builders && globalConfig.Converters, &Converter{config: config, tmpl: tmpl}),
+		// common.If[languages.Context](!config.SkipRuntime && globalConfig.Builders && globalConfig.Converters, &Converter{config: config, tmpl: tmpl}),
 	)
 	jenny.AddPostprocessors(common.GeneratedCommentHeader(globalConfig))
 
