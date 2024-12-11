2c05e5359d36ceb4dfdf1658f3ac0bee39d88f37
Kévin Gomez <kevin.gomez@grafana.com>
2024-12-11 22:20:27 +0100

    Fix Go converters for map of builders

--- internal/jennies/golang/converter.go
+++ internal/jennies/golang/converter.go
@@ -83,11 +83,10 @@ func (jenny *Converter) generateConverter(context languages.Context, builder ast
 			"importStdPkg": func(pkg string) string {
 				return imports.Add(pkg, pkg)
 			},
-			"importPkg":           typeImportMapper,
-			"formatType":          builderTypeFormatter(jenny.Config, context, dummyImports, dummyImportMapper).formatType,
-			"formatTypeNoBuilder": defaultTypeFormatter(jenny.Config, context, dummyImports, dummyImportMapper).formatType,
-			"formatPath":          makePathFormatter(formatter),
-			"formatRawRef":        formatRawRef,
+			"importPkg":    typeImportMapper,
+			"formatType":   builderTypeFormatter(jenny.Config, context, dummyImports, dummyImportMapper).formatType,
+			"formatPath":   makePathFormatter(formatter),
+			"formatRawRef": formatRawRef,
 		}).
 		RenderAsBytes("converters/converter.tmpl", map[string]any{
 			"Imports":   imports,
