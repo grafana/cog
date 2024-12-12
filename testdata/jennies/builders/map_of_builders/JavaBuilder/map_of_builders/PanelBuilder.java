03ef2c9ec28c8fa01cea536a69f99e29216fe78f
spinillos <selenepinillos@gmail.com>
2024-12-12 17:24:03 +0100

    Update tests

---
+++ testdata/jennies/builders/map_of_builders/JavaBuilder/map_of_builders/PanelBuilder.java
@@ -0,0 +1,17 @@
+package map_of_builders;
+
+
+public class PanelBuilder<T extends PanelBuilder<T>> implements cog.Builder<Panel> {
+    protected final Panel internal;
+    
+    public PanelBuilder() {
+        this.internal = new Panel();
+    }
+    public T title(String title) {
+    this.internal.title = title;
+        return (T) this;
+    }
+    public Panel build() {
+        return this.internal;
+    }
+}
