5e3f0bce85edba2b3f3255c85314332df16974f9
Selene <selenepinillos@gmail.com>
2024-12-18 10:14:35 +0100

    Java: Fixes maps (#690)
    * Fixes Java maps
    
    * Linter fixse
    
    * Fix typo and HashMap import

--- internal/jennies/java/types.go
+++ internal/jennies/java/types.go
@@ -59,12 +59,15 @@ func (tf *typeFormatter) resolvesToComposableSlot(typeDef ast.Type) bool {
 }
 
 func (tf *typeFormatter) formatBuilderFieldType(def ast.Type) string {
-	value := tf.formatFieldType(def)
 	if tf.resolvesToComposableSlot(def) || tf.typeHasBuilder(def) {
-		value = fmt.Sprintf("%s.Builder<%s>", tf.config.formatPackage("cog"), value)
+		if def.Kind == ast.KindMap {
+			tf.packageMapper("java.util", "Map")
+			return fmt.Sprintf("Map<String, %s.Builder<%s>>", tf.config.formatPackage("cog"), tf.formatFieldType(def.AsMap().ValueType))
+		}
+		return fmt.Sprintf("%s.Builder<%s>", tf.config.formatPackage("cog"), tf.formatFieldType(def))
 	}
 
-	return value
+	return tf.formatFieldType(def)
 }
 
 func (tf *typeFormatter) formatReference(def ast.RefType) string {
@@ -144,7 +147,7 @@ func (tf *typeFormatter) defaultValueFor(def ast.Type) string {
 		return "new LinkedList<>()"
 	case ast.KindMap:
 		tf.packageMapper("java.util", "HashMap")
-		return "new Hashmap<>()"
+		return "new HashMap<>()"
 	case ast.KindRef:
 		refDef := fmt.Sprintf("%s.%s", def.AsRef().ReferredPkg, def.AsRef().ReferredType)
 		if tf.typeHasBuilder(def) {
--- testdata/jennies/builders/map_of_builders/JavaBuilder/map_of_builders/DashboardBuilder.java
+++ testdata/jennies/builders/map_of_builders/JavaBuilder/map_of_builders/DashboardBuilder.java
@@ -1,6 +1,7 @@
 package map_of_builders;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class DashboardBuilder implements cog.Builder<Dashboard> {
     protected final Dashboard internal;
@@ -8,8 +9,12 @@ public class DashboardBuilder implements cog.Builder<Dashboard> {
     public DashboardBuilder() {
         this.internal = new Dashboard();
     }
-    public DashboardBuilder panels(cog.Builder<Map<String, Panel>> panels) {
-    this.internal.panels = panels.build();
+    public DashboardBuilder panels(Map<String, cog.Builder<Panel>> panels) {
+        Map<String, Panel> panelsResource = new HashMap<>();
+        for (var entry : panels.entrySet()) {
+           panelsResource.put(entry.getKey(), entry.getValue().build());
+        }
+    this.internal.panels = panelsResource;
         return this;
     }
     public Dashboard build() {
