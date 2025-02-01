657a4c372fe78769eda52e75f24b5c84fd573b8b
Kévin Gomez <kevin.gomez@grafana.com>
2025-02-01 02:07:18 +0100

    Fix unmarshalling of maps in Python

--- internal/jennies/python/tmpl.go
+++ internal/jennies/python/tmpl.go
@@ -51,8 +51,11 @@ func initTemplates(apiRefCollector *common.APIReferenceCollector, extraTemplates
 			"importPkg": func(alias string, pkg string) string {
 				panic("importPkg() needs to be overridden by a jenny")
 			},
-			"disjunctionFromJSON": func(typeDef ast.Type, inputVar string) disjunctionFromJSONCode {
+			"disjunctionFromJSON": func(typeDef ast.Type, inputVar string, hint string) fromJSONCode {
 				panic("disjunctionFromJSON() needs to be overridden by a jenny")
+			},
+			"unmarshalForType": func(typeDef ast.Type, inputVar string, hint string) fromJSONCode {
+				panic("unmarshalForType() needs to be overridden by a jenny")
 			},
 		}),
 
