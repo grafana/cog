561bb190fd1274c78ef5f5e61ea9c3979c1eb1c1
Kévin Gomez <kevin.gomez@grafana.com>
2025-02-12 23:56:32 +0100

    Update examples for changes in kind-registry next

--- examples/_go/main.go
+++ examples/_go/main.go
@@ -18,8 +18,7 @@ func dashboardBuilder() []byte {
 		Timezone(common.TimeZoneBrowser).
 		Timepicker(
 			dashboard.NewTimePickerBuilder().
-				RefreshIntervals([]string{"5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"}).
-				TimeOptions([]string{"5m", "15m", "1h", "6h", "12h", "24h", "2d", "7d", "30d"}),
+				RefreshIntervals([]string{"5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"}),
 		).
 		Tooltip(dashboard.DashboardCursorSyncCrosshair).
 		// "Data Source" variable
