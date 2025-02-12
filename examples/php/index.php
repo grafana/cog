8ea15ac994313a756b9ac487bb37652a54be06df
Kévin Gomez <kevin.gomez@grafana.com>
2025-02-12 23:56:32 +0100

    Update examples for changes in kind-registry next

--- examples/php/index.php
+++ examples/php/index.php
@@ -27,9 +27,7 @@ $builder = (new DashboardBuilder(title: '[TEST] Node Exporter / Raspberry'))
     ->time('now-30m', 'now')
     ->timezone(Common\Constants::TIME_ZONE_BROWSER)
     ->timepicker(
-        (new TimePickerBuilder())
-            ->refreshIntervals(['5s', '10s', '30s', '1m', '5m', '15m', '30m', '1h', '2h', '1d'])
-            ->timeOptions(['5m', '15m', '1h', '6h', '12h', '24h', '2d', '7d', '30d'])
+        (new TimePickerBuilder())->refreshIntervals(['5s', '10s', '30s', '1m', '5m', '15m', '30m', '1h', '2h', '1d'])
     )
     ->tooltip(DashboardCursorSync::crosshair())
     // 'Data Source' variable
