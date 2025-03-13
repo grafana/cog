ee3c96d7e7d25a0b1e4b40dfcf49e781420ef231
Kévin Gomez <kevin.gomez@grafana.com>
2025-03-13 16:58:21 +0100

    Simplify test case for AllowMarshalEmptyDisjunctions Go jenny option

--- internal/jennies/golang/rawtypes_test.go
+++ internal/jennies/golang/rawtypes_test.go
@@ -55,20 +55,11 @@ func TestRawTypes_Generate(t *testing.T) {
 }
 
 func TestRawTypes_Generate_AllowMarshalEmptyDisjunctions(t *testing.T) {
-	test := testutils.GoldenFilesTestSuite[ast.Schema]{
-		TestDataRoot: "../../../testdata/jennies/rawtypes",
-		Name:         "GoRawTypesAllowMarshalEmptyDisjunctions",
-		Skip: map[string]string{
-			"dashboard": "the dashboard test schema includes a composable slot, which rely on external input to be properly supported",
-		},
-	}
+	req := require.New(t)
 
 	config := Config{
 		PackageRoot:                   "github.com/grafana/cog/generated",
-		GenerateEqual:                 true,
 		GenerateJSONMarshaller:        true,
-		GenerateStrictUnmarshaller:    true,
-		GenerateValidate:              true,
 		AllowMarshalEmptyDisjunctions: true,
 	}
 	jenny := RawTypes{
@@ -78,23 +69,21 @@ func TestRawTypes_Generate_AllowMarshalEmptyDisjunctions(t *testing.T) {
 	}
 	compilerPasses := New(config).CompilerPasses()
 
-	test.Run(t, func(tc *testutils.Test[ast.Schema]) {
-		req := require.New(tc)
+	schema := &ast.Schema{
+		Package: "tests",
+		Objects: testutils.ObjectsMap(
+			ast.NewObject("tests", "DisjunctionOfScalars", ast.NewDisjunction(ast.Types{ast.String(), ast.Bool()})),
+		),
+	}
+	schemas, err := compilerPasses.Process([]*ast.Schema{schema})
+	req.NoError(err)
 
-		// We run the compiler passes defined fo Go since without them, we
-		// might not be able to translate some of the IR's semantics into Go.
-		// Example: disjunctions.
-		schema := tc.UnmarshalJSONInput(testutils.RawTypesIRInputFile)
-		processedAsts, err := compilerPasses.Process(ast.Schemas{&schema})
-		req.NoError(err)
+	context := languages.Context{
+		Schemas: schemas,
+	}
 
-		req.Len(processedAsts, 1, "we somehow got more ast.Schema than we put in")
+	result, err := jenny.generateSchema(context, schemas[0])
+	req.NoError(err)
 
-		files, err := jenny.Generate(languages.Context{
-			Schemas: processedAsts,
-		})
-		req.NoError(err)
-
-		tc.WriteFiles(files)
-	})
+	req.NotContains(string(result), "no value for disjunction of scalars")
 }
