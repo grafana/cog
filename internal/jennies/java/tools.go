f6bf51f2496f1f2ed90047aead20a7bee6c374ae
spinillos <selenepinillos@gmail.com>
2025-06-20 08:38:53 +0200

    Add Java custom unmarshallers

--- internal/jennies/java/tools.go
+++ internal/jennies/java/tools.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/grafana/cog/internal/ast"
+	"github.com/grafana/cog/internal/jennies/template"
 	"github.com/grafana/cog/internal/languages"
 	"github.com/grafana/cog/internal/tools"
 )
@@ -166,7 +167,7 @@ func getJavaFieldTypeCheck(t ast.Type) string {
 	}
 }
 
-func objectNeedsCustomDeserialiser(context languages.Context, obj ast.Object) bool {
+func objectNeedsCustomDeserialiser(context languages.Context, obj ast.Object, tmpl *template.Template) bool {
 	// an object needs a custom unmarshal if:
 	// - it is a struct that was generated from a disjunction by the `DisjunctionToType` compiler pass.
 	// - it is a struct and one or more of its fields is a KindComposableSlot, or an array of KindComposableSlot
@@ -175,8 +176,13 @@ func objectNeedsCustomDeserialiser(context languages.Context, obj ast.Object) bo
 		return false
 	}
 
+	// is there a custom unmarshal template block?
+	if tmpl.Exists(template.CustomObjectUnmarshalBlock(obj)) {
+		return true
+	}
+
 	// is it a struct generated from a disjunction?
-	if obj.Type.IsStructGeneratedFromDisjunction() {
+	if obj.Type.IsDisjunctionOfAnyKind() {
 		return true
 	}
 
