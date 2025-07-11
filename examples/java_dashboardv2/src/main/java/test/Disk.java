3e081f4d611d0952873a1189920587af5a2dd67d
spinillos <selenepinillos@gmail.com>
2025-07-11 10:06:58 +0200

    Update java

--- examples/java_dashboardv2/src/main/java/test/Disk.java
+++ examples/java_dashboardv2/src/main/java/test/Disk.java
@@ -10,6 +10,7 @@ import com.grafana.foundation.dashboardv2alpha1.DynamicConfigValue;
 import com.grafana.foundation.dashboardv2alpha1.PanelBuilder;
 import com.grafana.foundation.dashboardv2alpha1.QueryGroupBuilder;
 import com.grafana.foundation.dashboardv2alpha1.TargetBuilder;
+import com.grafana.foundation.dashboardv2alpha1.TransformationBuilder;
 import com.grafana.foundation.table.TableVizConfigKindBuilder;
 import com.grafana.foundation.units.Constants;
 
@@ -63,15 +64,87 @@ public class Disk {
                                                                 new DynamicConfigValue("max", 1))))
                                 .data(new QueryGroupBuilder()
                                                 .targets(List.of(
-                                                                new TargetBuilder().query(Common.basicPrometheusQuery(
-                                                                                "rate(node_disk_read_bytes_total{job=\"integrations/raspberrypi-node\", instance=\"$instance\", device!=\"\"}[$__rate_interval])",
-                                                                                "{{device}} read")),
-                                                                new TargetBuilder().query(Common.basicPrometheusQuery(
-                                                                                "rate(node_disk_written_bytes_total{job=\"integrations/raspberrypi-node\", instance=\"$instance\", device!=\"\"}[$__rate_interval])",
-                                                                                "{{device}} written")),
-                                                                new TargetBuilder().query(Common.basicPrometheusQuery(
-                                                                                "rate(node_disk_io_time_seconds_total{job=\"integrations/raspberrypi-node\", instance=\"$instance\", device!=\"\"}[$__rate_interval])",
-                                                                                "{{device}} read")))));
+                                                                new TargetBuilder().refId("A")
+                                                                                .query(Common.basicPrometheusQuery(
+                                                                                                "max by (mountpoint) (node_filesystem_size_bytes{job=\"integrations/raspberrypi-node\", instance=\"$instance\", fstype!=\"\"})",
+                                                                                                "A")),
+                                                                new TargetBuilder().refId("B")
+                                                                                .query(Common.basicPrometheusQuery(
+                                                                                                "max by (mountpoint) (node_filesystem_avail_bytes{job=\"integrations/raspberrypi-node\", instance=\"$instance\", fstype!=\"\"})",
+                                                                                                "B"))))
+                                                .transformation(new TransformationBuilder()
+                                                                .kind("groupBy")
+                                                                .id("groupBy")
+                                                                .options(transformer1Options()))
+                                                .transformation(new TransformationBuilder().kind("merge").id("merge")
+                                                                .options(Map.of()))
+                                                .transformation(new TransformationBuilder().kind("calculateField")
+                                                                .id("calculateField")
+                                                                .options(transformer2Options()))
+                                                .transformation(new TransformationBuilder().kind("calculateField")
+                                                                .id("calculateField")
+                                                                .options(transformer3Options()))
+                                                .transformation(new TransformationBuilder().kind("organize")
+                                                                .id("organize")
+                                                                .options(transformer4Options()))
+                                                .transformation(new TransformationBuilder().kind("sortBy")
+                                                                .id("sortBy")
+                                                                .options(transformer5Options())));
 
+        }
+
+        private static Map<String, Object> transformer1Options() {
+                return Map.of(
+                                "fields", Map.of(
+                                                "Value #A", Map.of(
+                                                                "aggregations", List.of("lastNotNull"),
+                                                                "operation", "aggregate"),
+                                                "Value #B", Map.of(
+                                                                "aggregations", List.of("lastNotNull"),
+                                                                "operation", "aggregate"),
+                                                "mountpoint", Map.of(
+                                                                "aggregations", List.of(),
+                                                                "operation", "groupby")));
+
+        }
+
+        private static Map<String, Object> transformer2Options() {
+                return Map.of(
+                                "alias", "Used",
+                                "binary", Map.of(
+                                                "left", "Value #A (lastNotNull)",
+                                                "operator", "-",
+                                                "reducer", "sum",
+                                                "right", "Value #B (lastNotNull)"),
+                                "mode", "binary",
+                                "reduce", Map.of("reducer", "sum"));
+        }
+
+        private static Map<String, Object> transformer3Options() {
+                return Map.of(
+                                "alias", "Used, %",
+                                "binary", Map.of(
+                                                "left", "Used",
+                                                "operator", "/",
+                                                "reducer", "sum",
+                                                "right", "Value #A (lastNotNull)"),
+                                "mode", "binary",
+                                "reduce", Map.of("reducer", "sum"));
+        }
+
+        private static Map<String, Object> transformer4Options() {
+                return Map.of(
+                                "excludeByName", List.of(),
+                                "indexByName", List.of(),
+                                "renameByName", Map.of(
+                                                "Value #A (lastNotNull)", "Size",
+                                                "Value #B (lastNotNull)", "Available",
+                                                "mountpoint", "Mounted on"));
+        }
+
+        private static Map<String, Object> transformer5Options() {
+                return Map.of(
+                                "fields", List.of(),
+                                "sort", Map.of("field", "Mounted on"));
         }
 }
