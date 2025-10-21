f714b6dc317e5c71240a2b55d4fa756876ab871e
spinillos <selenepinillos@gmail.com>
2025-10-21 13:58:04 +0200

    Update tests

--- testdata/jennies/builders/map_of_disjunctions/JavaBuilder/map_of_disjunctions/PanelBuilder.java
+++ testdata/jennies/builders/map_of_disjunctions/JavaBuilder/map_of_disjunctions/PanelBuilder.java
@@ -1,16 +1,16 @@
 package map_of_disjunctions;
 
 
-public class PanelBuilder<T extends PanelBuilder<T>> implements cog.Builder<Panel> {
+public class PanelBuilder implements cog.Builder<Panel> {
     protected final Panel internal;
     
     public PanelBuilder() {
         this.internal = new Panel();
         this.internal.kind = "Panel";
     }
-    public T title(String title) {
+    public PanelBuilder title(String title) {
         this.internal.title = title;
-        return (T) this;
+        return this;
     }
     public Panel build() {
         return this.internal;
