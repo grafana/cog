d5187bc692fae3e80b635bbf7eee85bd6b90e483
spinillos <selenepinillos@gmail.com>
2026-01-21 12:47:30 +0100

    Fix Java map assignments and examples

--- examples/_go_dashboardv2/cpu.go
+++ examples/_go_dashboardv2/cpu.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/grafana/cog/generated/go/cog"
 	"github.com/grafana/cog/generated/go/common"
 	dashboard "github.com/grafana/cog/generated/go/dashboardv2beta1"
 	"github.com/grafana/cog/generated/go/units"
@@ -25,9 +26,8 @@ func cpuUsageTimeseries() *dashboard.PanelBuilder {
 					dashboard.NewThresholdsConfigBuilder().
 						Mode(dashboard.ThresholdsModeAbsolute).
 						Steps([]dashboard.Threshold{
-							// TODO: shouldn't value be nullable?
-							{Value: 0, Color: "green"},
-							{Value: 80.0, Color: "red"},
+							{Value: cog.ToPtr[float64](0), Color: "green"},
+							{Value: cog.ToPtr[float64](80.0), Color: "red"},
 						}),
 				),
 		).
@@ -48,9 +48,8 @@ func loadAverageTimeseries() *dashboard.PanelBuilder {
 					dashboard.NewThresholdsConfigBuilder().
 						Mode(dashboard.ThresholdsModeAbsolute).
 						Steps([]dashboard.Threshold{
-							// TODO: shouldn't value be nullable?
-							{Value: 0, Color: "green"},
-							{Value: 80.0, Color: "red"},
+							{Value: cog.ToPtr[float64](0), Color: "green"},
+							{Value: cog.ToPtr[float64](80.0), Color: "red"},
 						}),
 				).
 				Min(0).
@@ -85,10 +84,9 @@ func cpuTemperatureGauge() *dashboard.PanelBuilder {
 					dashboard.NewThresholdsConfigBuilder().
 						Mode(dashboard.ThresholdsModeAbsolute).
 						Steps([]dashboard.Threshold{
-							// TODO: shouldn't value be nullable?
-							{Value: 0, Color: "rgba(50, 172, 45, 0.97)"},
-							{Value: 65.0, Color: "rgba(237, 129, 40, 0.89)"},
-							{Value: 85.0, Color: "rgba(245, 54, 54, 0.9)"},
+							{Value: cog.ToPtr[float64](0), Color: "rgba(50, 172, 45, 0.97)"},
+							{Value: cog.ToPtr[float64](65.0), Color: "rgba(237, 129, 40, 0.89)"},
+							{Value: cog.ToPtr[float64](85.0), Color: "rgba(245, 54, 54, 0.9)"},
 						}),
 				),
 		).
