42af34da51b4746295862070529a00a7a2d961c3
Selene <selenepinillos@gmail.com>
2026-01-30 13:18:15 +0100

    Veneers: Allow package wildcard in selectors (#944)
    * Allow wildcard in omit option
    
    * Add omit in by_name selector

--- internal/veneers/option/selectors.go
+++ internal/veneers/option/selectors.go
@@ -21,7 +21,7 @@ func EveryOption() Selector {
 // Note: the comparison on object and options names is case-insensitive.
 func ByName(pkg string, objectName string, optionNames ...string) Selector {
 	return func(builder ast.Builder, option ast.Option) bool {
-		return builder.For.SelfRef.ReferredPkg == pkg &&
+		return (builder.For.SelfRef.ReferredPkg == pkg || pkg == "*") &&
 			strings.EqualFold(builder.For.Name, objectName) &&
 			tools.StringInListEqualFold(option.Name, optionNames)
 	}
@@ -31,7 +31,7 @@ func ByName(pkg string, objectName string, optionNames ...string) Selector {
 // Note: the comparison on builder and options names is case-insensitive.
 func ByBuilder(pkg string, builderName string, optionNames ...string) Selector {
 	return func(builder ast.Builder, option ast.Option) bool {
-		return builder.Package == pkg &&
+		return (builder.Package == pkg || pkg == "*") &&
 			strings.EqualFold(builder.Name, builderName) &&
 			tools.StringInListEqualFold(option.Name, optionNames)
 	}
