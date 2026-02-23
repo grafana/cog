31ad4415a6a438b0f5ca2c1ba11df181f12261aa
Kévin Gomez <kevin.gomez@grafana.com>
2026-02-23 13:01:29 +0100

    Descriptive option selectors

--- internal/veneers/option/selectors.go
+++ internal/veneers/option/selectors.go
@@ -1,38 +1,59 @@
 package option
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/grafana/cog/internal/ast"
 	"github.com/grafana/cog/internal/tools"
 )
 
-type Selector func(builder ast.Builder, option ast.Option) bool
+type Selector struct {
+	description string
+	matcher     func(builder ast.Builder, option ast.Option) bool
+}
+
+func (selector Selector) Matches(builder ast.Builder, option ast.Option) bool {
+	return selector.matcher(builder, option)
+}
+
+func (selector Selector) String() string {
+	return selector.description
+}
 
 // EveryOption accepts any given option.
-func EveryOption() Selector {
-	return func(_ ast.Builder, _ ast.Option) bool {
-		return true
+func EveryOption() *Selector {
+	return &Selector{
+		description: "every_option",
+		matcher: func(_ ast.Builder, _ ast.Option) bool {
+			return true
+		},
 	}
 }
 
 // ByName matches options by their name, defined for builders for the given
 // object (referred to by its package and name).
 // Note: the comparison on object and options names is case-insensitive.
-func ByName(pkg string, objectName string, optionNames ...string) Selector {
-	return func(builder ast.Builder, option ast.Option) bool {
-		return (builder.For.SelfRef.ReferredPkg == pkg || pkg == "*") &&
-			strings.EqualFold(builder.For.Name, objectName) &&
-			tools.StringInListEqualFold(option.Name, optionNames)
+func ByName(pkg string, objectName string, optionNames ...string) *Selector {
+	return &Selector{
+		description: fmt.Sprintf("by_name[builder.for.pkg='%s', option.name=(%s)]", objectName, strings.Join(optionNames, ", ")),
+		matcher: func(builder ast.Builder, option ast.Option) bool {
+			return (builder.For.SelfRef.ReferredPkg == pkg || pkg == "*") &&
+				strings.EqualFold(builder.For.Name, objectName) &&
+				tools.StringInListEqualFold(option.Name, optionNames)
+		},
 	}
 }
 
 // ByBuilder matches options by their name and the name of the builder containing them..
 // Note: the comparison on builder and options names is case-insensitive.
-func ByBuilder(pkg string, builderName string, optionNames ...string) Selector {
-	return func(builder ast.Builder, option ast.Option) bool {
-		return (builder.Package == pkg || pkg == "*") &&
-			strings.EqualFold(builder.Name, builderName) &&
-			tools.StringInListEqualFold(option.Name, optionNames)
+func ByBuilder(pkg string, builderName string, optionNames ...string) *Selector {
+	return &Selector{
+		description: fmt.Sprintf("by_name[builder.name='%s', option.name=(%s)]", builderName, strings.Join(optionNames, ", ")),
+		matcher: func(builder ast.Builder, option ast.Option) bool {
+			return (builder.Package == pkg || pkg == "*") &&
+				strings.EqualFold(builder.Name, builderName) &&
+				tools.StringInListEqualFold(option.Name, optionNames)
+		},
 	}
 }
