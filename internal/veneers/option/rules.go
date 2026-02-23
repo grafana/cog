5338731287470e101ca4a6cd57826db2955c43ca
Kévin Gomez <kevin.gomez@grafana.com>
2026-02-23 13:54:45 +0100

    Fix docs generation for builders/options transformations

--- internal/veneers/option/rules.go
+++ internal/veneers/option/rules.go
@@ -34,6 +34,7 @@ func (action Action) String() string {
 	return action.description
 }
 
+// Rename renames an option.
 func Rename(selector *Selector, newName string) Rule {
 	return Rule{
 		Selector: selector,
@@ -44,6 +45,27 @@ func Rename(selector *Selector, newName string) Rule {
 	}
 }
 
+// ArrayToAppend updates the option to perform an "append" assignment.
+//
+// Example:
+//
+//	```
+//	func Tags(tags []string) {
+//		this.resource.tags = tags
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func Tags(tags string) {
+//		this.resource.tags.append(tags)
+//	}
+//	```
+//
+// This action returns the option unchanged if:
+//   - it doesn't have exactly one argument
+//   - the argument is not an array
 func ArrayToAppend(selector *Selector) Rule {
 	return Rule{
 		Selector: selector,
@@ -54,6 +76,27 @@ func ArrayToAppend(selector *Selector) Rule {
 	}
 }
 
+// MapToIndex updates the option to perform an "index" assignment.
+//
+// Example:
+//
+//	```
+//	func Elements(elements map[string]Element) {
+//		this.resource.elements = elements
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func Elements(key string, elements Element) {
+//		this.resource.elements[key] = tags
+//	}
+//	```
+//
+// This action returns the option unchanged if:
+//   - it doesn't have exactly one argument
+//   - the argument is not a map
 func MapToIndex(selector *Selector) Rule {
 	return Rule{
 		Selector: selector,
@@ -64,6 +107,7 @@ func MapToIndex(selector *Selector) Rule {
 	}
 }
 
+// RenameArguments renames the arguments of an options.
 func RenameArguments(selector *Selector, newNames []string) Rule {
 	return Rule{
 		Selector: selector,
@@ -74,6 +118,7 @@ func RenameArguments(selector *Selector, newNames []string) Rule {
 	}
 }
 
+// Omit removes an option.
 func Omit(selector *Selector) Rule {
 	return Rule{
 		Selector: selector,
@@ -94,6 +139,27 @@ func VeneerTrailAsComments(selector *Selector) Rule {
 	}
 }
 
+// UnfoldBoolean transforms an option accepting a boolean argument into two argument-less options.
+//
+// Example:
+//
+//	```
+//	func Editable(editable bool) {
+//		this.resource.editable = editable
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func Editable() {
+//		this.resource.editable = true
+//	}
+//
+//	func ReadOnly() {
+//		this.resource.editable = false
+//	}
+//	```
 func UnfoldBoolean(selector *Selector, unfoldOpts BooleanUnfold) Rule {
 	return Rule{
 		Selector: selector,
@@ -104,6 +170,33 @@ func UnfoldBoolean(selector *Selector, unfoldOpts BooleanUnfold) Rule {
 	}
 }
 
+// StructFieldsAsArguments uses the fields of the first argument's struct (assuming it is one) and turns them
+// into arguments.
+//
+// Optionally, an explicit list of fields to turn into arguments can be given.
+//
+// Example:
+//
+//	```
+//	func Time(time {from string, to string) {
+//		this.resource.time = time
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func Time(from string, to string) {
+//		this.resource.time.from = from
+//		this.resource.time.to = to
+//	}
+//	```
+//
+// This action returns the option unchanged if:
+//   - it has no arguments
+//   - the first argument is not a struct or a reference to one
+//
+// FIXME: considers the first argument only.
 func StructFieldsAsArguments(selector *Selector, explicitFields ...string) Rule {
 	return Rule{
 		Selector: selector,
@@ -114,6 +207,36 @@ func StructFieldsAsArguments(selector *Selector, explicitFields ...string) Rule 
 	}
 }
 
+// StructFieldsAsOptions uses the fields of the first argument's struct (assuming it is one) and turns them
+// into options.
+//
+// Optionally, an explicit list of fields to turn into options can be given.
+//
+// Example:
+//
+//	```
+//	func GridPos(gridPos {x int, y int) {
+//		this.resource.gridPos = gridPos
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func X(x int) {
+//		this.resource.gridPos.x = x
+//	}
+//
+//	func Y(y int) {
+//		this.resource.gridPos.y = y
+//	}
+//	```
+//
+// This action returns the option unchanged if:
+//   - it has no arguments
+//   - the first argument is not a struct or a reference to one
+//
+// FIXME: considers the first argument only.
 func StructFieldsAsOptions(selector *Selector, explicitFields ...string) Rule {
 	return Rule{
 		Selector: selector,
@@ -124,6 +247,32 @@ func StructFieldsAsOptions(selector *Selector, explicitFields ...string) Rule {
 	}
 }
 
+// DisjunctionAsOptions uses the branches of the first argument's disjunction (assuming it is one) and turns them
+// into options.
+//
+// Example:
+//
+//	```
+//	func Panel(panel Panel|Row) {
+//		this.resource.panels.append(panel)
+//	}
+//	```
+//
+// Will become:
+//
+//	```
+//	func Panel(panel Panel) {
+//		this.resource.panels.append(panel)
+//	}
+//
+//	func Row(row Row) {
+//		this.resource.panels.append(row)
+//	}
+//	```
+//
+// This action returns the option unchanged if:
+//   - it has no arguments
+//   - the given argument is not a disjunction or a reference to one
 func DisjunctionAsOptions(selector *Selector, argumentIndex int) Rule {
 	return Rule{
 		Selector: selector,
@@ -144,6 +293,7 @@ func Duplicate(selector *Selector, duplicateName string) Rule {
 	}
 }
 
+// AddAssignment adds an assignment to an existing option.
 func AddAssignment(selector *Selector, assignment veneers.Assignment) Rule {
 	return Rule{
 		Selector: selector,
@@ -154,6 +304,7 @@ func AddAssignment(selector *Selector, assignment veneers.Assignment) Rule {
 	}
 }
 
+// AddComments adds comments to an option.
 func AddComments(selector *Selector, comments []string) Rule {
 	return Rule{
 		Selector: selector,
@@ -164,6 +315,7 @@ func AddComments(selector *Selector, comments []string) Rule {
 	}
 }
 
+// Debug prints debugging information about an option.
 func Debug(selector *Selector) Rule {
 	return Rule{
 		Selector: selector,
