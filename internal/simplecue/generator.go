fcc3509e7d93f5ae23021af36b42b8a797b02952
spinillos <selenepinillos@gmail.com>
2026-04-01 13:46:27 +0200

    Update cue parser to identify list constraints

--- internal/simplecue/generator.go
+++ internal/simplecue/generator.go
@@ -961,6 +961,53 @@ func extractNumber(e cueast.Expr) (string, bool) {
 	return "", false
 }
 
+func (g *generator) declareListConstraints(v cue.Value) ([]ast.TypeConstraint, error) {
+	conjuncts := appendSplit(nil, cue.AndOp, v)
+
+	if len(conjuncts) == 1 {
+		return nil, nil
+	}
+
+	var constraints []ast.TypeConstraint
+
+	for _, conjunct := range conjuncts {
+		op, args := conjunct.Expr()
+		if op != cue.CallOp {
+			continue
+		}
+
+		switch fmt.Sprint(args[0]) {
+		case "list.MinItems":
+			scalar, err := cueConcreteToScalar(args[1])
+			if err != nil {
+				return nil, err
+			}
+			constraints = append(constraints, ast.TypeConstraint{
+				Op:   ast.MinItemsOp,
+				Args: []any{scalar},
+			})
+
+		case "list.MaxItems":
+			scalar, err := cueConcreteToScalar(args[1])
+			if err != nil {
+				return nil, err
+			}
+			constraints = append(constraints, ast.TypeConstraint{
+				Op:   ast.MaxItemsOp,
+				Args: []any{scalar},
+			})
+
+		case "list.UniqueItems":
+			constraints = append(constraints, ast.TypeConstraint{
+				Op:   ast.UniqueItemsOp,
+				Args: nil,
+			})
+		}
+	}
+
+	return constraints, nil
+}
+
 func (g *generator) declareList(v cue.Value, defVal any, hints ast.JenniesHints) (ast.Type, error) {
 	typeDef := ast.NewArray(ast.Any(), ast.Hints(hints), ast.Default(defVal))
 
@@ -981,6 +1028,12 @@ func (g *generator) declareList(v cue.Value, defVal any, hints ast.JenniesHints)
 		_, dvals := v.Expr()
 		v = dvals[0]
 	}
+
+	constraints, err := g.declareListConstraints(v)
+	if err != nil {
+		return ast.Type{}, err
+	}
+	typeDef.Array.Constraints = constraints
 
 	e := v.LookupPath(cue.MakePath(cue.AnyIndex))
 	if !e.Exists() {
