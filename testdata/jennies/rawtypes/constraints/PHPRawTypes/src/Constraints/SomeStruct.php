f8aceb09666081b4f0c7b5fd86c325e5fd7b4380
spinillos <selenepinillos@gmail.com>
2026-04-01 13:46:41 +0200

    Update languages and tests

--- testdata/jennies/rawtypes/constraints/PHPRawTypes/src/Constraints/SomeStruct.php
+++ testdata/jennies/rawtypes/constraints/PHPRawTypes/src/Constraints/SomeStruct.php
@@ -29,6 +29,21 @@ class SomeStruct implements \JsonSerializable
     public string $negativeRegex;
 
     /**
+     * @var array<string>
+     */
+    public array $minMaxList;
+
+    /**
+     * @var array<string>
+     */
+    public array $uniqueList;
+
+    /**
+     * @var array<int>
+     */
+    public array $fullConstraintList;
+
+    /**
      * @param int|null $id
      * @param int|null $maybeId
      * @param int|null $greaterThanZero
@@ -38,8 +53,11 @@ class SomeStruct implements \JsonSerializable
      * @param array<string>|null $tags
      * @param string|null $regex
      * @param string|null $negativeRegex
+     * @param array<string>|null $minMaxList
+     * @param array<string>|null $uniqueList
+     * @param array<int>|null $fullConstraintList
      */
-    public function __construct(?int $id = null, ?int $maybeId = null, ?int $greaterThanZero = null, ?int $negative = null, ?string $title = null, ?array $labels = null, ?array $tags = null, ?string $regex = null, ?string $negativeRegex = null)
+    public function __construct(?int $id = null, ?int $maybeId = null, ?int $greaterThanZero = null, ?int $negative = null, ?string $title = null, ?array $labels = null, ?array $tags = null, ?string $regex = null, ?string $negativeRegex = null, ?array $minMaxList = null, ?array $uniqueList = null, ?array $fullConstraintList = null)
     {
         $this->id = $id ?: 0;
         $this->maybeId = $maybeId;
@@ -50,6 +68,9 @@ class SomeStruct implements \JsonSerializable
         $this->tags = $tags ?: [];
         $this->regex = $regex ?: "";
         $this->negativeRegex = $negativeRegex ?: "";
+        $this->minMaxList = $minMaxList ?: [];
+        $this->uniqueList = $uniqueList ?: [];
+        $this->fullConstraintList = $fullConstraintList ?: [];
     }
 
     /**
@@ -57,7 +78,7 @@ class SomeStruct implements \JsonSerializable
      */
     public static function fromArray(array $inputData): self
     {
-        /** @var array{id?: int, maybeId?: int, greaterThanZero?: int, negative?: int, title?: string, labels?: array<string, string>, tags?: array<string>, regex?: string, negativeRegex?: string} $inputData */
+        /** @var array{id?: int, maybeId?: int, greaterThanZero?: int, negative?: int, title?: string, labels?: array<string, string>, tags?: array<string>, regex?: string, negativeRegex?: string, minMaxList?: array<string>, uniqueList?: array<string>, fullConstraintList?: array<int>} $inputData */
         $data = $inputData;
         return new self(
             id: $data["id"] ?? null,
@@ -69,6 +90,9 @@ class SomeStruct implements \JsonSerializable
             tags: $data["tags"] ?? null,
             regex: $data["regex"] ?? null,
             negativeRegex: $data["negativeRegex"] ?? null,
+            minMaxList: $data["minMaxList"] ?? null,
+            uniqueList: $data["uniqueList"] ?? null,
+            fullConstraintList: $data["fullConstraintList"] ?? null,
         );
     }
 
@@ -86,6 +110,9 @@ class SomeStruct implements \JsonSerializable
         $data->tags = $this->tags;
         $data->regex = $this->regex;
         $data->negativeRegex = $this->negativeRegex;
+        $data->minMaxList = $this->minMaxList;
+        $data->uniqueList = $this->uniqueList;
+        $data->fullConstraintList = $this->fullConstraintList;
         if (isset($this->maybeId)) {
             $data->maybeId = $this->maybeId;
         }
